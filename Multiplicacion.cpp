dfab61ee4f3ad161ac1f6d5c35a84e8fa90f7c0b
facumarin <ma_facu@hotmail.com>
2016-05-05 07:28:09 -0300

    Added files via upload

---
+++ Multiplicacion.cpp
@@ -0,0 +1,24 @@
+/* 
+Apellido y Nombre: Marin Facundo 
+Curso: K1051 
+Legajo: 158.829-1 
+Fecha: 05/05/2016 
+*/ 
+
+#include <iostream>
+
+	int main(){
+	
+	int a,b;
+	int c = 0;
+	int i = 0;
+	std::cout << "ingrese el primer valor: ";
+	std::cin >> a;
+	std::cout << "ingrese el segundo valor: ";
+	std::cin >> b;
+if (i!=a)
+do {c=c+b;
+i=i+1;
+} while(i!=a);
+std::cout << "el resultado es: " << c;
+}
